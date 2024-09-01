ab75ab5d46e7d5e5595d9c45a0c3e5013956d248
willykar <wilsonnjenga799@gmail.com>
2024-09-01 13:29:05 -0700

    db.js

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,6 @@
-import mongodb from 'mongodb';
-import Collection from 'mongodb/lib/collection';
-import envLoader from './env_loader';
+import mongodb from 'mongodb'
+import Collection from 'mongodb/lib/collection'
+import envLoader from './env_loader'
 
 /**
  * Represents a MongoDB client.
