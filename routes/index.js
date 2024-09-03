853689c31019e8e34b3922929d67d1d7fcb6ca77
willykar <103494595+willykar@users.noreply.github.com>
2024-09-04 02:05:38 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import { Express } from 'express';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
