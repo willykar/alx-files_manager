c49e77348205ca72e96b87c0e7df010bdc876f21
willykar <103494595+willykar@users.noreply.github.com>
2024-09-05 01:36:05 +0300

    Update UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,39 +1,44 @@
-import sha1 from 'sha1';
-import Queue from 'bull/lib/queue';
-import dbClient from '../utils/db';
+const { ObjectId } = require('mongodb');
+const sha1 = require('sha1');
+const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
 
-const userQueue = new Queue('email sending');
+class UsersController {
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    let userId = await redisClient.get(`auth_${token}`);
+    console.log(`user id: ${userId}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    userId = new ObjectId(userId);
+    const user = await (await dbClient.usersCollection()).findOne({ _id: userId });
+    const data = { email: user.email, id: user._id };
 
-export default class UsersController {
+    return res.status(200).json(data);
+  }
+
   static async postNew(req, res) {
-    const email = req.body ? req.body.email : null;
-    const password = req.body ? req.body.password : null;
-
+    const { email, password } = req.body;
     if (!email) {
-      res.status(400).json({ error: 'Missing email' });
-      return;
+      return res.status(400).json({ error: 'Missing email' });
     }
     if (!password) {
-      res.status(400).json({ error: 'Missing password' });
-      return;
+      return res.status(400).json({ error: 'Missing password' });
     }
     const user = await (await dbClient.usersCollection()).findOne({ email });
-
     if (user) {
-      res.status(400).json({ error: 'Already exist' });
-      return;
+      return res.status(400).json({ error: 'Already exist' });
     }
-    const insertionInfo = await (await dbClient.usersCollection())
-      .insertOne({ email, password: sha1(password) });
-    const userId = insertionInfo.insertedId.toString();
+    const hashedPassword = sha1(password);
+    const result = await (await dbClient.usersCollection())
+      .insertOne({ email, password: hashedPassword });
 
-    userQueue.add({ userId });
-    res.status(201).json({ email, id: userId });
-  }
-
-  static async getMe(req, res) {
-    const { user } = req;
-
-    res.status(200).json({ email: user.email, id: user._id.toString() });
+    return res.status(201).json({ id: result.insertedId, email });
   }
 }
+
+module.exports = UsersController;
